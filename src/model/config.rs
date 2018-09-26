f11e0323619c1b1b5b703a683bd865b7ab8db6db
rrredface <theredfaceone@gmail.com>
2018-09-26 03:21:49 -0500

    Add permissions system for commands

--- src/model/config.rs
+++ src/model/config.rs
@@ -4,6 +4,8 @@ use std::io::{Read, Write};
 use std::fs::OpenOptions;
 use std::path::Path;
 use super::error::Error;
+use std::collections::BTreeMap;
+use serenity::model::guild::PartialMember;
 use toml; // cargo spews errors without this line
 
 /// A representation of the options that can be changed for the bot.
@@ -23,6 +25,8 @@ pub struct Config {
     pub channel_vote: u64,
     /// The Discord channel ID in which keynote members can vote.
     pub channel_keynote: u64,
+    /// The permissions section.
+    pub permissions: BTreeMap<String, Vec<String>>,
 }
 
 impl Default for Config {
@@ -35,6 +39,7 @@ impl Default for Config {
             channel_announce: 0,
             channel_vote: 0,
             channel_keynote: 1, // this needs to be different from channel_vote
+            permissions: Default::default(),
         }
     }
 }
@@ -68,6 +73,16 @@ impl Config {
         // Write content to file.
         Ok(file.write_all(&content.as_bytes())?)
     }
+    /// Check if a user has a permission.
+    pub fn user_has_perm(&self, member: &PartialMember, value: &str) -> bool {
+        for role in member.roles.iter() {
+            let id = role.as_u64().to_string();
+            if let Some(group) = &self.permissions.get(&id) {
+                if group.contains(&value.to_string()) {
+                    return true
+                }
+            }
+        }
+        false
+    }
 }
-
-
