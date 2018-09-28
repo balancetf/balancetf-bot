d8283cd7f13081312e1d4876c876a57d3e7e54c3
rrredface <theredfaceone@gmail.com>
2018-09-27 19:25:28 -0500

    Updated structs for new role scheme in Discord. (#9)

--- src/model/vote.rs
+++ src/model/vote.rs
@@ -3,38 +3,32 @@
 pub struct Vote {
     /// Vote ID.
     pub id: u64,
-    /// Number of yes votes.
-    pub yes: u64,
-    /// Number of no votes.
-    pub no: u64,
+    /// Number of yes votes from users with the Casual role.
+    pub casual_yes: u64,
+    /// Number of no votes from users with the Casual role.
+    pub casual_no: u64,
+    /// Number of yes votes from users with the Competitive role.
+    pub comp_yes: u64,
+    /// Number of no votes from users with the Competitive role.
+    pub comp_no: u64,
     /// The time at which voting closes and the results are final.
     pub end_time: u64,
 }
 
 impl Vote {
     /// Creates a new vote with the ID `id`, which ends at `end_time`
-    fn new(id: u64, end_time: u64) -> Self {
+    pub fn new(id: u64, end_time: u64) -> Self {
         Vote {
             id,
-            yes: 0,
-            no: 0,
-            end_time
+            casual_yes: 0,
+            casual_no: 0,
+            comp_yes: 0,
+            comp_no: 0,
+            end_time,
         }
     }
-    /// Increments the counter for yes votes.
-    fn add_yes(&mut self) {
-        self.yes = self.yes + 1;
-    }
-    /// Decrements the coutner for yes votes.
-    fn rm_yes(&mut self) {
-        self.yes = self.yes - 1;
-    }
-    /// Increments the counter for no votes.
-    fn add_no(&mut self) {
-        self.no = self.no + 1;
-    }
-    /// Decrements the counter for no votes.
-    fn rm_no(&mut self) {
-        self.no = self.no - 1;
+    /// Gets the total yes and no votes, regardless of casual/comp.
+    pub fn total(&self) -> (u64, u64) {
+        (self.casual_yes + self.comp_yes, self.casual_no + self.comp_no)
     }
 }
